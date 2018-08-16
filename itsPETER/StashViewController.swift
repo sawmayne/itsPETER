7ae3b3822fcd45cfc15e489c8db74fd16664795e
Peter Gow <petergow@Peters-MacBook-Pro.local>
2018-08-17 03:29:19 +1000

    Timer logic complete, ready for controller logic.

---
+++ itsPETER/StashViewController.swift
@@ -0,0 +1,35 @@
+//
+//  StashViewController.swift
+//  itsPETER
+//
+//  Created by Peter Gow on 17/8/18.
+//  Copyright © 2018 SamWayne. All rights reserved.
+//
+
+import UIKit
+
+class StashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
